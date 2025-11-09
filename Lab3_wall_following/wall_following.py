2e73e8c63dd1042ff2fae809c16226042aaac2f6
Faheemibrahim <Faheem1243@gmail.com>
2025-11-10 04:13:32 +0530

    previous commit was garbage (working code but need update)

--- Lab3_wall_following/wall_following.py
+++ Lab3_wall_following/wall_following.py
@@ -12,83 +12,139 @@ class WallFollow(Node):
     def __init__(self):
         super().__init__('wall_follow_node')
 
-        lidarscan_topic = '/scan'
-        drive_topic = '/drive'
+        #publisher 
+        self.drive_publisher = self.create_publisher(
+            AckermannDriveStamped,
+            '/drive',
+            10)
+        
+        #subscriber
+        self.scan_callback_handle = self.create_subscription(
+            LaserScan,
+            '/scan',
+            self.scan_callback,
+            10)
+        
+        # set PID gains (needs tuning)
+        self.kp = 1.0
+        self.ki = 0
+        self.kd = 0.1
+        self.wind_up = 1.0  # anti-windup limit
 
-        # TODO: create subscribers and publishers
+        # wall following params
+        self.l = 0.1 # lookahead distance (play with this to see how the car oscillates) reduce to reduce oscillations increase to make car more responsive
+        self.desired_distance = 1.0 # (m) desired distance from wall
+        self.speed = 0.0  # initial speed (m/s)
 
-        # TODO: set PID gains
-        # self.kp = 
-        # self.kd = 
-        # self.ki = 
+        #steering angle 
+        self.steering_angle = 0.0
+        self.max_steering_angle = 0.4189  # rad
+        
+        # PID state (starts at zero)
+        self.integral = 0.0
+        self.prev_error = 0.0
+        self.error = 0.0
+        self.PID = 0.0
 
-        # TODO: store history
-        # self.integral = 
-        # self.prev_error = 
-        # self.error = 
-
+        # time variables for discrete time PID
+        self.prev_time = None
+        
+        # lidar params
+        self.range_data = [] # lidare cashe distance data
+        self.angle_min = 0.0
+        self.angle_increment = 0.0
+        
         # TODO: store any necessary values you think you'll need
 
-    def get_range(self, range_data, angle):
-        """
-        Simple helper to return the corresponding range measurement at a given angle. Make sure you take care of NaNs and infs.
+    def scan_callback(self, msg: LaserScan):
 
-        Args:
-            range_data: single range array from the LiDAR
-            angle: between angle_min and angle_max of the LiDAR
+        # gets msg from lidar and converted to parameters
+        self.range_data = msg.ranges  # lidar all the distances 
+        self.angle_min = msg.angle_min  # start angle of the scan [rad]
+        self.angle_max = msg.angle_max  # end angle of the scan [rad]
+        self.angle_increment = msg.angle_increment  # angular distance between measurements [rad]
 
-        Returns:
-            range: range measurement in meters at the given angle
+        # Compute control + publish
+        self.get_error()      
+        self.pid_control()   
+        self.steer()           
 
-        """
+    def get_distance(self,angle):
 
-        #TODO: implement
-        return 0.0
+        index = int((angle - self.angle_min) / self.angle_increment)
+        if 0 <= index < len(self.range_data):
+            distance = self.range_data[index]
+               
+            if np.isnan(distance) or np.isinf(distance): 
+                return 10.0
+            else:
+                return distance
+        else:        
+            return 10.0
 
-    def get_error(self, range_data, dist):
-        """
-        Calculates the error to the wall. Follow the wall to the left (going counter clockwise in the Levine loop). You potentially will need to use get_range()
+    def get_error(self):        
 
-        Args:
-            range_data: single range array from the LiDAR
-            dist: desired distance to the wall
+        a_angle = 60 * np.pi/180
+        b_angle = 20 * np.pi/180
+        theta = a_angle - b_angle # angle is (40) fixed 
 
-        Returns:
-            error: calculated error
-        """
+        a_distance = self.get_distance(a_angle) 
+        b_distance = self.get_distance(b_angle)  
+        
+        alpha = np.arctan(( a_distance*np.cos(theta) - b_distance )/( a_distance*np.sin(theta))) # 1 (check this value for max)
+        D_t = b_distance * np.cos(alpha) # 2 perpendicular distance to wall
+        D_t_plus_1 = D_t + self.l * np.sin(alpha) # 3 lookahead distance
 
-        #TODO:implement
-        return 0.0
+        self.error = self.desired_distance - D_t_plus_1 #4
 
-    def pid_control(self, error, velocity):
-        """
-        Based on the calculated error, publish vehicle control
+        return alpha
 
-        Args:
-            error: calculated error
-            velocity: desired velocity
+    def pid_control(self):
 
-        Returns:
-            None
-        """
-        angle = 0.0
-        # TODO: Use kp, ki & kd to implement a PID controller
+        # calculate (dt) as system is descrete time (If needed add EMA(Exponential Moving Average) to smooth dt)
+        current_time = self.get_clock().now().nanoseconds / 1e9  # 1second (convert to nanoseconds)
+        if self.prev_time is None:
+            dt = 0.004                                      #  250 Hz clock rate
+        else:
+            dt = current_time - self.prev_time
+            dt = float(np.clip(dt, 0.005, 0.05))
+        self.prev_time = current_time
+
+        P = self.kp * self.error  
+
+        self.integral += self.error * dt # integral of error over time
+        I = self.ki * max(min(self.integral, self.wind_up), -self.wind_up)  # anti-windup
+
+        derivation = (self.error - self.prev_error)/dt  # rate of change of error over time
+        D = self.kd * derivation
+        self.prev_error = self.error
+
+        self.PID = P + I + D
+
+        return self.PID
+
+
+
+    def steer(self):
+    
+        self.steering_angle = np.clip(self.PID, -1.0, 1.0) * self.max_steering_angle
+
+        ten_deg = np.deg2rad(10.0)
+        twenty_deg = np.deg2rad(20.0)
+        abs_sa = abs(self.steering_angle) # temporary absolute-value copy for checking how large the turn is
+
+        if 0 < abs_sa < ten_deg:
+            self.speed = 1.5
+        elif ten_deg < abs_sa < twenty_deg:
+            self.speed = 1.0
+        else:
+            self.speed = 0.5
+
+        # publishes msg to drive topic
         drive_msg = AckermannDriveStamped()
-        # TODO: fill in drive message and publish
-
-    def scan_callback(self, msg):
-        """
-        Callback function for LaserScan messages. Calculate the error and publish the drive message in this function.
-
-        Args:
-            msg: Incoming LaserScan message
-
-        Returns:
-            None
-        """
-        error = 0.0 # TODO: replace with error calculated by get_error()
-        velocity = 0.0 # TODO: calculate desired car velocity based on error
-        self.pid_control(error, velocity) # TODO: actuate the car with PID
+        drive_msg.drive.speed = self.speed
+        drive_msg.drive.steering_angle = -self.steering_angle # added negative sign to steer correctly
+        self.drive_publisher.publish(drive_msg)
 
 
 def main(args=None):
